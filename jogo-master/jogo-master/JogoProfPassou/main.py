0bafd88103128fd8a3bf8807145038c487657e74
KatsS2 <102443448+KatssII@users.noreply.github.com>
2023-05-15 11:01:45 -0300

    Add files via upload
    09/06/22

---
+++ jogo-master/jogo-master/JogoProfPassou/main.py
@@ -0,0 +1,111 @@
+import random
+
+import pygame
+import sys
+from jogo import controles as ctrl, grafiocs as g
+
+
+width, height = 800, 600
+
+coluna_pista = 0
+linha_pista = 0
+
+pygame.init()
+
+screen=pygame.display.set_mode((width,height))
+
+pygame.display.set_caption("Meu primeiro jogo")
+
+#fundo_path = "img/pista.jpg"
+carro_path = "img/carro1.png"
+
+#fundo = pygame.image.load(fundo_path)
+#fundo = pygame.transform.scale(fundo, (800, 600))
+
+carro1 = pygame.image.load(carro_path)
+carro2 = pygame.image.load(carro_path)
+carro3 = pygame.image.load(carro_path)
+carro4 = pygame.image.load(carro_path)
+
+# define a posição do carro na tela.
+coluna, linha = 0, 0
+
+#screen.blit(fundo, (0, 0) )
+
+screen.fill((0,0,0))
+pygame.display.flip()
+
+# mover a pista para baixo
+def mover_fundo(position=(coluna_pista, linha_pista)):
+    if position[0] >= 600:
+        position[0] = -10
+    screen.blit(fundo, position)
+
+def escrever_texto(posicao=(0,0), texto="texto", corTexto=(0,0,0), corFundo=(255,255,255)):
+    font = pygame.font.Font('freesansbold.ttf', 22)
+    #t = font.render(texto, True, (255,255,220), (0,190,190))
+    t = font.render(texto, True, corTexto, corFundo)
+    textRect = t.get_rect()
+    textRect.center = posicao
+    screen.blit(t,textRect)
+
+def desenha_retangulo(cor=(255,255,255), rect=(230, 230, 50, 200) ):
+    pygame.draw.rect(screen, cor, pygame.Rect(rect))
+
+def desenha_circulo(cor=(255,255,255), pos=(10,10)):
+    pygame.draw.circle(screen, cor, pos, 100, 50 )
+
+
+coluna_carro2 = 450
+linha_carro2 = 200
+
+while True:
+
+    linha_carro2 -= 1
+
+    if linha_carro2 < -650:
+        linha_carro2 = +850
+
+    # desenha_circulo(cor=(255,255,0), pos=(550,300))
+    # desenha_circulo( pos=(500,400) )
+    # desenha_circulo( cor=(0,0,255), pos=(600, 400))
+
+    # for x in range(1):
+    #     l = random.randint(100,600)
+    #     c = random.randint(100,800)
+    #     color_handle_r = random.randint(0, 255)
+    #     color_handle_g = random.randint(0, 255)
+    #     color_handle_b = random.randint(0, 255)
+    #     desenha_circulo(cor=(color_handle_r, color_handle_g, color_handle_b), pos=(c, l))
+
+    s = "[{0},{1}]".format(ctrl.coluna, ctrl.linha)
+    escrever_texto( (720,18), s)
+
+    keys = pygame.key.get_pressed()
+    ctrl.ctrl_key(keys)
+
+    # screen.fill((0, 0, 0), fundo.get_rect())
+    screen.fill((0, 0, 0))
+
+    pygame.draw.circle(screen,
+                     (random.randint(0,255),
+                      random.randint(0,255),
+                      random.randint(0,255)), (random.randint(0,800),random.randint(0,600)),4 )
+    linha_pista += .9
+    if linha_pista > 600: linha_pista = - 250
+
+    desenha_retangulo(cor=(255, 255, 255), rect=((screen.get_width() - 25) / 2, linha_pista, 25, 200))
+
+    screen.blit(carro1, (ctrl.coluna, ctrl.linha))
+    screen.blit(carro2, (coluna_carro2, linha_carro2))
+    screen.blit(carro3, (coluna_carro2 - 300, linha_carro2 - 150 ))
+    screen.blit(carro4, (coluna_carro2 +  100, linha_carro2 + 220 ))
+
+    pygame.display.flip()
+
+    #linha_pista += 100
+
+    for eve in pygame.event.get():
+        if eve.type==pygame.QUIT:
+           pygame.quit()
+           sys.exit()
